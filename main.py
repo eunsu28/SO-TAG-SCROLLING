1edf4e4d6eb0bc7501bf291e12eb56d549a7e677
eunsu20191222 <eunsuvg@gmail.com>
2021-06-06 22:25:30 +0800

    start

---
+++ main.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+
+limited = 50
+url = "https://stackoverflow.com/questions/tagged/beautifulsoup"
+
+result = requests.get(url)
+soup = BeautifulSoup(result.text, "html.parser")
+pages = soup.find("div", {"class": "s-pagination"}).find_all("a")
+last_pages = pages[-1].get_text(strip=True)
+final = int(last_pages)
+
+def extract_job(html):
+  title = html.find("div", {"class":"summary"}).find("a")["question-hyperlink"]
+  job_id = html.find("a")
+
+
+jobs = []
+for page in range(final):
+    print(f"Scrapping SO Page: {page}")
+    result = requests.get(f"{url}&pg={page + 1}")
+    soup = BeautifulSoup(result.text, "html.parser")
+    results = soup.find_all("div", {"class":"-job"})
+    for result in results:
+       job = extract_job(result)
+       jobs.append(job)
