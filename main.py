fe672128e090daed56f27d59e667ee271799874e
eunsu20191222 <eunsuvg@gmail.com>
2021-06-07 14:02:30 +0800

    에러

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
 
-limited = 50
-url = "https://stackoverflow.com/questions/tagged/beautifulsoup"
 
-result = requests.get(url)
-soup = BeautifulSoup(result.text, "html.parser")
-pages = soup.find("div", {"class": "s-pagination"}).find_all("a")
-last_pages = pages[-1].get_text(strip=True)
-int(last_pages)
+url = "https://stackoverflow.com/questions/tagged/beautifulsoup?tab=newest&pagesize=50"
 
 def extract_job(html):
   title = html.find("div", {"class":"summary"}).find("a")["question-hyperlink"]
   job_id = html.find("a")
 
 
+result = requests.get(url)
+soup = BeautifulSoup(result.text, "html.parser")
+pages = soup.find("div", {"class": "s-pagination"}).find_all("a")
+last_pages = pages[-1].get_text(strip=True)
+final = int(last_pages)
+
+
+print(type(final))
+
 jobs = []
-for page in range(last_pages):
+for page in range(int(final)):
     print(f"Scrapping SO Page: {page}")
     result = requests.get(f"{url}&pg={page + 1}")
     soup = BeautifulSoup(result.text, "html.parser")
@@ -25,4 +28,3 @@ for page in range(last_pages):
        job = extract_job(result)
        jobs.append(job)
 
-print(jobs)
